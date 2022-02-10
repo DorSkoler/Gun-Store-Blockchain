0c54a77021f18e2d76a2e391d09ae81656b32758
shahafmeron7 <shahafmeron@gmail.com>
2022-02-10 21:35:49 +0200

    dor

--- client/src/weapons/trainingPrices.js
+++ client/src/weapons/trainingPrices.js
@@ -1,40 +1,38 @@
-export default [
+export default {
     //------Cold-----//
-    {
-        type: "Cold",
-        shooting_range: "0.0001",
+    "Cold": {
+        0: "0.0001",
         basic_traning: "0.0002",
         advanced_traning: "0.0003",
         idle: "0.00005"
     },
-    {
-        type: "Heavy",
+    "Heavy":{
         shooting_range: "0.005",
         basic_traning: "0.008",
         advanced_traning: "0.01",
         idle: "0.005"
-    },{
-        type: "Light",
+    },
+    "Light":{
         shooting_range: "0.001",
         basic_traning: "0.002",
         advanced_traning: "0.003",
         idle: "0.0015"
-    },{
-        type: "Pistol",
+    },
+    "Pistol":{
         shooting_range: "0.00024",
         basic_traning: "0.00069",
         advanced_traning: "0.00089",
         idle: "0.0005"
-    },{
-        type: "Shotgun",
+    },
+    "Shotgun":{
         shooting_range: "0.0015",
         basic_traning: "0.0045",
         advanced_traning: "0.0067",
         idle: "0.005"
-    },{
-        type: "Sniper",
+    },
+    "Sniper":{
         shooting_range: "0.025",
         basic_traning: "0.04",
         advanced_traning: "0.08",
         idle: "0.05"
-    }]
+    }}
