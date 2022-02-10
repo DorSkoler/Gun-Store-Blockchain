5956c816d404028344a2106db8e2ebdfda7ce519
shahafmeron7 <shahafmeron@gmail.com>
2022-02-10 23:42:52 +0200

    test

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(express.json())
 
-//DB connection
+//DB connectionn
 mongoose.connect(process.env.DB_URI, {useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
     console.log("DB connected");
