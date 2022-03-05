d80a78d18c78f4e86488d43123f823974624b075
Dor Skoler <dorskoler@gmail.com>
2022-03-05 11:04:03 +0200

    new contract

--- smartContract/contracts/Transactions.sol
+++ smartContract/contracts/Transactions.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 TsxCount;
 
     event Transfer(address from, address receiver, uint amount,uint256 timestamp, string weapon, string weaponType,string weaponUrl);
   
@@ -18,9 +17,8 @@ contract Transactions {
     }
 
     Ts[] transactions;
-    //need to check params for new transaction.
+
     function addToBlockchain(address payable receiver, uint amount, string memory weapon,string memory weaponType,string memory weaponUrl) public {
-        TsxCount += 1;
         transactions.push(Ts(msg.sender, receiver, amount, block.timestamp, weapon,weaponType, weaponUrl));
         emit Transfer(msg.sender, receiver, amount, block.timestamp, weapon,weaponType, weaponUrl);
     }
@@ -30,8 +28,4 @@ contract Transactions {
 
     }
 
-    function getTransactionCount() public view returns (uint256) {
-        return TsxCount;
-    }
 }
-//contract address 0x676f7d285a88E9025Bd3430Ac4a31B35434F6842
